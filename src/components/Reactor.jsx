b5453c7ca8a48a7382ce1ae7167ccbe4d38c501b
Rajkumar-Developer <rajkumarrcse28@gmail.com>
2026-08-20 00:33:48 +0530

    altering the reactor position and size

--- src/components/Reactor.jsx
+++ src/components/Reactor.jsx
@@ -3,7 +3,13 @@ import { useGLTF } from '@react-three/drei'
 function Reactor() {
   const { scene } = useGLTF('/models/reactor/reactor.glb')
 
-  return <primitive object={scene} scale={1.2}/>
+  return (
+    <primitive
+      object={scene}
+      position={[0, 0, 0]}
+      scale={1.2}
+    />
+  )
 }
 
 useGLTF.preload('/models/reactor/reactor.glb')
