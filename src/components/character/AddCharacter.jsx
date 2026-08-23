f95b9659eec7062ca6581cb48950da8f5bb26a60
Rajkumar-Developer <rajkumarrcse28@gmail.com>
2026-08-23 23:35:01 +0530

    character size change

--- src/components/character/AddCharacter.jsx
+++ src/components/character/AddCharacter.jsx
@@ -4,7 +4,7 @@ function AddCharacter(){
     const { scene } = useGLTF('/models/character/demoChar.glb');
 
     return (
-        <primitive object={scene} position={[0,2,0]}/>
+        <primitive object={scene} position={[0,1.5,0]} scale={1.9}/>
     );
 }
 useGLTF.preload('/models/character/demoChar.glb');
