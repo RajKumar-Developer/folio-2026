0c92848c87e0fa41f457bdab974d22e2d67bc80a
Rajkumar-Developer <rajkumarrcse28@gmail.com>
2026-08-30 17:59:28 +0530

    setting glsl

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glsl from 'vite-plugin-glsl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    glsl({
+      include: '**/*.glsl',
+      defaultExtension: 'glsl'
+    })
+  ]
 })
